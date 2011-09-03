fadace7cc93312f8d51aae22e82f9e64b02c9201
Kishore Vinnakota <kishorevk@gmail.com>
2011-09-02 23:44:11 -0400

    Initial bpo commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Safebpo::Application.config.secret_token = '276fd6ed9589837853ad991c5ee0ee7216873cd33584242e9e28ff55695eaaac9639c0025b08e8f8f2c0a9063e20480bec6866a72bbc0d8e288a7b263935a00b'
